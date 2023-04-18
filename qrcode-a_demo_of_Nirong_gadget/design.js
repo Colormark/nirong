baffc68f16eb05893d8f7b3440052d16f83d3796
Colormark <10973837@qq.com>
2023-04-18 23:22:45 +0800

    Add files via upload

--- qrcode-a_demo_of_Nirong_gadget/design.js
+++ qrcode-a_demo_of_Nirong_gadget/design.js
@@ -1,10 +1,10 @@
 module.exports = function(){
+    // console.log(this);
+    NR(this).on("click", function($gadget, instanceInfo){
+        // console.log($gadget, instanceInfo)
+        $gadget.addClass('animate__animated animate__flipOutX').delay(500).queue(function() {
+            $(this).removeClass('animate__animated animate__flipOutX').dequeue();
+        })
 
-    const gadgetInfo = this;
-    
-    const $cartButton = NRD.renderGadgetCartButton(gadgetInfo["gadgetCartButtonIcon"], gadgetInfo);
-    $cartButton.on("click", function(){
-        NR.renderGadget(gadgetInfo);
-    });
-    
+    })
 }
